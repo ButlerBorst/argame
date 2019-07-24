b498ce7298cbfeb67774122f557155789f639563
github username <github email address>
2019-07-24 14:23:39 -0700

    testing cards

--- src/ArModels.js
+++ src/ArModels.js
@@ -158,20 +158,20 @@ class arModels extends Component {
 
 <a-light type="ambient" intensity="1">
 
-          <a-marker preset='custom' type='pattern' url='https://raw.githubusercontent.com/ButlerBorst/ar-project-glitch/master/assets/Tester2.patt'>
+          <a-marker preset='hiro'>
             <a-entity gltf-model='https://raw.githubusercontent.com/ButlerBorst/ar-project-glitch/master/assets/FITOKEN/FITOKEN.gltf' scale="-10 10 10" rotation="0 0 0" position="0 -1 0">
               <a-animation attribute="rotation" to="0 360 0" dur="9000" repeat="indefinite" easing="linear"></a-animation>
             </a-entity>
           </a-marker>
 
-          <a-marker preset='custom' type='pattern' url='https://raw.githubusercontent.com/ButlerBorst/ar-project-glitch/master/assets/pattern-Tester.patt'>
+          <a-marker preset='kanji'>
             <a-entity gltf-model='https://raw.githubusercontent.com/ButlerBorst/ar-project-glitch/master/assets/LearnCoSymb.gltf' scale="-10 10 10" rotation="0 0 0" position="0 -1 0">
               <a-animation attribute="rotation" to="0 360 0" dur="9000" repeat="indefinite" easing="linear"></a-animation>
             </a-entity>
           </a-marker>
 
 
-          <a-marker preset='hiro'>
+          <a-marker preset='custom' type='pattern' url='https://raw.githubusercontent.com/ButlerBorst/ar-project-glitch/master/assets/pattern-Sun-Marker-Colored.patt'>
             <a-entity gltf-model="https://raw.githubusercontent.com/ButlerBorst/ar-project-glitch/master/assets/NewSun/NewSun.gltf" scale="-10 10 10" rotation="0 0 0" position="0 -.1 0">
               <a-animation attribute="rotation" to="0 360 0" dur="60000" repeat="indefinite" easing="linear"></a-animation>
             </a-entity>
@@ -194,10 +194,10 @@ class arModels extends Component {
 
 
           //new black hole model
-
+https://raw.githubusercontent.com/ButlerBorst/ar-project-glitch/master/assets/pattern-black-hole-marker.patt
       <a-light type="point"  color="pink" position="-2 -90 0"></a-light>
         <a-light type="point" color="pink" position="2 90 0"></a-light>
-          <a-marker preset='kanji'>
+          <a-marker preset='custom' type='pattern' url='https://raw.githubusercontent.com/ButlerBorst/ar-project-glitch/master/assets/pattern-black-hole-marker.patt'>
 
             <a-entity gltf-model="https://raw.githubusercontent.com/ButlerBorst/ar-project-glitch/master/assets/Ring11/Ring11.gltf" material="side: double" scale="-9 9 9" rotation="0 0 0" position="0 -.5 0">
               <a-animation attribute="rotation" to="360 360 0" dur="100000" repeat="indefinite" easing="linear"></a-animation>
